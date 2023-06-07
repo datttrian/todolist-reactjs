a8c1bfbc8057f243475c1e73a5163d61b003d65d
Dat T. Tran <datttrian@gmail.com>
2023-06-07 08:06:49 -0400

    Add styles configuration

---
+++ src/styles/themes.js
@@ -0,0 +1,12 @@
+export const themes = [
+	{
+		backgroundColor: "#1D3557",
+		foregroundColor: "#001D3D",
+		primaryColor: "#FFD166",
+		headerColor: "#FFFFFF",
+		darkHeaderColor: "#000000",
+		addButtonColor: "#1982C4",
+		completeColor: "#06D6A0",
+		deleteColor: "#E63946",
+	},
+]
